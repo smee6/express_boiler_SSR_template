7c6f08766e9945427456f57cc161e64df20da02c
smee6 <smee6@naver.com>
2023-03-21 14:30:49 +0900

    공백

--- src/middlewares/authUser.js
+++ src/middlewares/authUser.js
@@ -1,8 +1,9 @@
 exports.userAuth = (req, res, next) => {
     //여기에 뭔가 로직을 넣어서 유저를 인증한다.
 
-    //ipBlackList에 있는 ip는 접근을 막는다.
     const ipBlackList = [""];
+    //ipBlackList에 있는 ip는 접근을 막는다.
+
     if (ipBlackList.includes(req.ip)) {
         return res.status(401).send({ err: "인증되지 않은 유저입니다." });
     }
