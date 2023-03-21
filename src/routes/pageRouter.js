597d9ba171109ba01a2931b9d16620284e1ce822
smee6 <smee6@naver.com>
2023-03-21 18:30:16 +0900

    주석삭제

--- src/routes/pageRouter.js
+++ src/routes/pageRouter.js
@@ -4,12 +4,6 @@ const pageRouter = Router();
 pageRouter.get("/*", (req, res) => {
     return res.sendFile("index.html", { root: "./src/public/" });
 });
-// pageRouter.get("/pag21", (req, res) => {
-//     return res.sendFile("index.html", { root: "./src/public/" });
-// });
-// pageRouter.get("/page2", (req, res) => {
-//     return res.sendFile("index.html", { root: "./src/public/" });
-// });
 
 module.exports = { pageRouter };
 
